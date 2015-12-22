e159562cdf86598a2239b9d059b66467544e19f9
tsuyoshiwada <v23.b.wada@gmail.com>
2015-12-23 03:38:42 +0900

    Fix pass a value from props

--- src/js/actions/editor.js
+++ src/js/actions/editor.js
@@ -1,14 +1,17 @@
+import localStorage from "store"
 import * as types from "../constants/action-types"
 
 export function initializeValue() {
   return {
-    type: types.EDITOR_INITIALIZE_VALUE
-  }
+    type: types.EDITOR_INITIALIZE_VALUE,
+    value: localStorage.get("value")
+  };
 }
 
 export function changeValue(value) {
+  localStorage.set("value", value);
   return {
     type: types.EDITOR_CHANGE_VALUE,
     value
-  }
+  };
 }
