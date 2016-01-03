143c5722ee901d9d07079178a19bbf9c3ce5c8dd
tsuyoshiwada <v23.b.wada@gmail.com>
2016-01-03 16:42:24 +0900

    Add application constants

---
+++ src/js/constants/application.js
@@ -0,0 +1,4 @@
+export const APP_URL = "http://directree.net/";
+export const APP_NAME = "Directree";
+export const APP_TITLE = "Directree - インデントテキストをツリー状に変換";
+export const APP_DESCRIPTION = "Directreeはインデントで表した階層をツリー状に変換するWebアプリです";
