2aacf327650a8ff7e753ff79f037798ddb4e1d5b
tsuyoshiwada <v23.b.wada@gmail.com>
2016-01-03 22:47:45 +0900

    Add object comparison utility

---
+++ src/js/utils/object-equals.js
@@ -0,0 +1,21 @@
+export default function objectEquals(x, y) {
+  if (x === y) return true;
+
+  if (!(x instanceof Object) || !(y instanceof Object)) return false;
+
+  if (x.constructor !== y.constructor) return false;
+
+  for (let p in x) {
+    if (!x.hasOwnProperty(p)) continue;
+    if (!y.hasOwnProperty(p)) return false;
+    if (x[p] === y[p]) continue;
+    if (typeof x[p] !== "object") return false;
+    if (!objectEquals(x[p], y[p])) return false;
+  }
+
+  for (let p in y) {
+    if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) return false;
+  }
+
+  return true;
+}
