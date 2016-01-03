e5ba96c8bb1497dd180e9616f5dcbc5d0678ac1f
tsuyoshiwada <v23.b.wada@gmail.com>
2016-01-03 22:50:20 +0900

    Update editor scroll at percentage value

--- src/js/components/editor.js
+++ src/js/components/editor.js
@@ -2,6 +2,7 @@ import React, {Component, PropTypes} from "react"
 import brace from "brace"
 import "brace/mode/text"
 import "brace/theme/tomorrow_night"
+import objectEquals from "../utils/object-equals"
 
 const CURSOR_POS = 1;
 
@@ -12,7 +13,14 @@ export default class Editor extends Component {
     value: PropTypes.string,
     tabSize: PropTypes.number,
     readOnly: PropTypes.bool,
-    scrollTop: PropTypes.number,
+    scroll: PropTypes.shape({
+      top: PropTypes.number,
+      left: PropTypes.number
+    }),
+    scrollPer: PropTypes.shape({
+      top: PropTypes.number,
+      left: PropTypes.number
+    }),
     onChange: PropTypes.func,
     onScroll: PropTypes.func
   }
@@ -31,13 +39,15 @@ export default class Editor extends Component {
       value,
       tabSize,
       readOnly,
-      scrollTop,
+      scroll,
+      scrollPer,
       onChange,
       onScroll
     } = this.props;
 
     const editor = brace.edit(name);
     const session = editor.getSession();
+    this.editor = editor;
 
     editor.setTheme("ace/theme/tomorrow_night");
     editor.renderer.setPadding(10);
@@ -47,14 +57,17 @@ export default class Editor extends Component {
     editor.setOption("showPrintMargin", false);
     session.setMode("ace/mode/text");
 
-    if (scrollTop != null) {
-      session.setScrollTop(scrollTop);
+    if (scroll != null) {
+      this.setScroll(scroll);
+    }
+
+    if (scrollPer != null) {
+      this.setScrollPer(scrollPer);
     }
 
     editor.on("change", ::this.handleChange);
-    session.on("changeScrollTop", ::this.handleScroll);
-
-    this.editor = editor;
+    session.on("changeScrollTop", ::this.handleScrollTop);
+    session.on("changeScrollLeft", ::this.handleScrollLeft);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -70,8 +83,12 @@ export default class Editor extends Component {
       editor.setOption("readOnly", nextProps.readOnly);
     }
 
-    if (nextProps.scrollTop !== oldProps.scrollTop) {
-      session.setScrollTop(nextProps.scrollTop);
+    if (!objectEquals(nextProps.scroll, oldProps.scroll)) {
+      this.setScroll(nextProps.scroll);
+    }
+
+    if (!objectEquals(nextProps.scrollPer, oldProps.scrollPer)) {
+      this.setScrollPer(nextProps.scrollPer);
     }
 
     if (this.editor.getValue() !== nextProps.value) {
@@ -93,16 +110,77 @@ export default class Editor extends Component {
     }
   }
 
-  handleScroll(top) {
-    if (this.props.onScroll) {
-      this.props.onScroll(top);
-    }
+  handleScrollTop(top) {
+    this.requestScroll("top", top, this.editor.getSession().getScrollLeft());
+  }
+
+  handleScrollLeft(left) {
+    this.requestScroll("top", this.editor.getSession().getScrollTop(), left);
+  }
+
+  requestScroll(direction, top, left) {
+    if (!this.props.onScroll) return;
+    const size = this.getSize();
+    const editorSize = this.getEditorSize();
+
+    this.props.onScroll(direction, {top, left}, {
+      top: Math.max(0, Math.min(100, top / (editorSize.height - size.height) * 100)),
+      left: Math.max(0, Math.min(100, left / (editorSize.width - size.width) * 100))
+    });
+  }
+
+  setScroll(scroll) {
+    const session = this.editor.getSession();
+    const newScroll = Object.assign({}, {
+      top: session.getScrollTop(),
+      left: session.getScrollLeft()
+    }, scroll);
+
+    session.setScrollTop(newScroll.top);
+    session.setScrollLeft(newScroll.left);
+  }
+
+  setScrollPer(scrollPer) {
+    const per = Object.assign({}, {top: 0, left: 0}, scrollPer);
+    const size = this.getSize();
+    const editorSize = this.getEditorSize();
+
+    this.setScroll({
+      top: (editorSize.height - size.height) * (scrollPer.top / 100),
+      left: (editorSize.width - size.width) * (scrollPer.left / 100)
+    });
+  }
+
+  getSize() {
+    const {editorElement} = this.refs;
+    return {
+      width: editorElement.clientWidth,
+      height: editorElement.clientHeight
+    };
+  }
+
+  getEditorSize() {
+    return {
+      width: this.getEditorWidth(),
+      height: this.getEditorHeight()
+    };
+  }
+
+  getEditorWidth() {
+    const {editor} = this;
+    return editor.renderer.content.clientWidth;
+  }
+
+  getEditorHeight() {
+    const {editor} = this;
+    return editor.getSession().getScreenLength() * editor.renderer.lineHeight + editor.renderer.scrollBarH.height;
   }
 
   render() {
     return (
       <div
         id={this.props.name}
+        ref="editorElement"
         className={this.props.className}>
       </div>
     );
