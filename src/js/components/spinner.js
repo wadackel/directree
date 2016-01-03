04df1040b7811ba67a693d6cf43617f3aea279f2
tsuyoshiwada <v23.b.wada@gmail.com>
2016-01-03 18:18:55 +0900

    Add spinner component

---
+++ src/js/components/spinner.js
@@ -0,0 +1,19 @@
+import React, {Component, PropTypes} from "react"
+
+export default class Spinner extends Component {
+  static propTypes = {
+    isShow: PropTypes.bool
+  }
+
+  static defaultProps = {
+    isShow: true
+  }
+
+  render() {
+    const {isShow} = this.props;
+    const className = `spinner${isShow ? " is-show" : ""}`;
+    return (
+      <div className={className}></div>
+    );
+  }
+}
